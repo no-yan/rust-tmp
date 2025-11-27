8b50aa6586cffe41d67b248a19ca6e033a73806e
no-yan <63000297+no-yan@users.noreply.github.com>
2025-11-27 23:37:07 +0900

    複数の演算子を追加

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,21 +1,38 @@
-#![feature(str_split_whitespace_remainder)]
+use std::error::Error;
+
 fn main() {
     // 標準入力から数値を受け取り、それらの合計を標準出力に出す
     let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
     let split = stdin.split_whitespace();
 
-    let sum = calc(split);
+    let Ok(sum) = calc(split) else {
+        return;
+    };
+
     println!("{}", sum);
 }
 
-fn calc<I, S>(input: I) -> i32
+fn calc<I, S>(mut input: I) -> Result<i32, Box<dyn Error>>
 where
     I: Iterator<Item = S>,
     S: AsRef<str>,
 {
-    input
-        .filter_map(|s: S| s.as_ref().parse::<i32>().ok())
-        .sum()
+    let left = input.next().unwrap();
+    let op = input.next().unwrap();
+    let right = input.next().unwrap();
+
+    let left = left.as_ref().parse::<i32>()?;
+    let right = right.as_ref().parse::<i32>()?;
+
+    let result = match op.as_ref() {
+        "+" => left + right,
+        "-" => left - right,
+        "*" => left * right,
+        "/" => left / right,
+        _ => 0,
+    };
+
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -25,7 +42,7 @@ mod tests {
     #[test]
     fn sum() {
         let input = "1 + 2".split_whitespace();
-        let res = calc(input);
+        let res = calc(input).unwrap();
 
         assert_eq!(res, 3);
     }
